b6516c9e0cb24bbe8d4f22c71097081da785b459
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-06-14 14:16:37 -0700

    The_Followers_of_Yahweh_Ben_Yahweh finished

---
+++ src/pages/(sous-page)/Contact_Us/Contact_Us.tsx
@@ -0,0 +1,9 @@
+export function Contact_Us() {
+  return (
+    <div className="contact-us-page">
+      <div id="pg-banner-contact" className="page-banner"></div>
+      Coucou 7
+    </div>
+  );
+}
+export default Contact_Us;
---
+++ src/pages/(sous-page)/The_Followers_of_Yahweh_Ben_Yahweh/The_Followers_of_Yahweh_Ben_Yahweh.tsx
@@ -0,0 +1,91 @@
+import "./style/the_Followers_of_Yahweh_Ben_Yahweh.css";
+// import "./style/responsive.css"
+export function The_Followers_of_Yahweh_Ben_Yahweh() {
+  return (
+    <div className="followers-page">
+      <div
+        id="pg-banner-fol"
+        className="page-banner bg-cover bg-center h-48"
+      ></div>
+      <div id="top-bar-gold-fol" className=""></div>
+      <div id="The_Followers_of_Yahweh_Ben_Yahweh">
+        <img
+          className="img-size"
+          src="/src/pages/(sous-page)/The_Followers_of_Yahweh_Ben_Yahweh/image/The_Followers_of_Yahweh_Ben_Yahweh.png"
+          alt="The Followers of Yahweh Ben Yahweh"
+        />
+      </div>
+      <div className="sec-bar-fol"></div>
+      <div className="sec-text pg-text-fmt">
+        <p>
+          The followers of <span className="David">יהוה בן יהוה</span> belong to
+          a special, privileged group. They are chosen by{" "}
+          <span className="David">יהוה</span> for salvation and eternal life.
+          They are this day, being inaugurated and placed into their rightful
+          positions of rulership.
+        </p>
+      </div>
+      <div className="sec-bar-fol"></div>
+      <div className="sec-text pg-text-fmt">
+        <p className="pg-text-ltr-sp-2">
+          The followers of <span className="David">יהוה בן יהוה</span> are
+          experiencing salvation, love, and divine protection from{" "}
+          <span className="David">יהוה בן יהוה</span>, their God, and His Son,{" "}
+          <span className="David">יהוה בן יהוה</span> (Proverbs 18:10).
+        </p>
+      </div>
+      <div className="sec-bar-fol"></div>
+      <div className="sec-text pg-text-fmt">
+        <p className="">
+          The followers of <span className="David">יהוה בן יהוה</span> are human
+          beings who actually invoke and summon their intellectual powers to
+          think and transform thoughts into organized actions, which produce
+          tangible effects. They are not lackadaisical. Many can testify to the
+          fact the followers of <span className="David">יהוה בן יהוה</span> are
+          learners, scholars, and pupils of Biblical principles. They are
+          partisans, friends, allies, and backers of{" "}
+          <span className="David">יהוה</span> and His laws. They satisfy the
+          requirements of man through their exemplifications of logical and
+          rational decisions.
+        </p>
+      </div>
+      <div className="sec-bar-fol"></div>
+      <div className="sec-text pg-text-fmt">
+        <p className="">
+          True followers of <span className="David">יהוה בן יהוה</span> adhere
+          to His message of salvation, which demonstrates good faith toward all
+          good people, without dishonesty, fraud, or deceit; thereby causing all
+          that believe in and practice morality to be lawful, just, upright,
+          open, straight forward, well-behaved, candid, ethical, and principled.
+          With these distinct qualities existing in one's character, the person
+          becomes an imitation or godly transcription of divinity, and is a
+          reflection of <span className="David">יהוה</span> and His Son,{" "}
+          <span className="David">יהוה בן יהוה</span>.
+        </p>
+      </div>
+      <div className="sec-bar-fol"></div>
+      <div className="sec-text pg-text-fmt">
+        <p className="pg-text-ltr-sp-9">
+          In short, the followers of <span className="David">יהוה בן יהוה</span>{" "}
+          are strongly affectionate and attached to any individual or body of
+          people who abide by moral principles. They also have a feeling of
+          benevolence and brotherhood that all people should have for one
+          another. In fact, they are keenly interested in each other's well
+          being. They are society's true humanitarians: those who are active and
+          open participants in the building of the
+        </p>
+        <p className="pg-text-fmt-cnt">
+          Kingdom of <span className="David">יהוה</span> — Heaven.
+        </p>
+      </div>
+      <div id="pg-btm-mediax" className="">
+        <img
+          className="bottom-media-img"
+          src="/src/pages/(sous-page)/The_Followers_of_Yahweh_Ben_Yahweh/image/bottom-media.jpg"
+          alt="Bottom Media"
+        />
+      </div>
+    </div>
+  );
+}
+export default The_Followers_of_Yahweh_Ben_Yahweh;
