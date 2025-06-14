462ef1c3bbc16ae32e3335656452cf5760880daf
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-06-14 12:11:15 -0700

    About_Yahweh_Ben_Yahweh finished

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import UniverseOfYahweh from "./pages/(sous-page)/UniverseOfYahweh/UniverseOfYahweh";
 import CulturalAttire from "./pages/(sous-page)/CulturalAttire/CulturalAttire";
 import About_Yahweh from "./pages/(sous-page)/About_Yahweh/About_Yahweh";
+import AboutYahwehBenYahweh from "./pages/(sous-page)/About_Yahweh_Ben_Yahweh/About_Yahweh_Ben_Yahweh";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/Cultural_Attire" element={<CulturalAttire />} />
             {/* Ajoutez d'autres routes selon vos besoins */}
             <Route path="/About_Yahweh" element={<About_Yahweh />} />
+            <Route path="/About_Yahweh_Ben_Yahweh" element={<AboutYahwehBenYahweh />} />
           </Routes>
         </Layout>
       </Router>
---
+++ src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/About_Yahweh_Ben_Yahweh.tsx
@@ -0,0 +1,140 @@
+import "./style/about_Yahweh_Ben_Yahweh.css";
+import "./style/responsive.css";
+export function AboutYahwehBenYahweh() {
+  return (
+    <div className="about-yahweh-ben-yahweh-page">
+      <div id="pg-banner-about-y" className="page-banner"></div>
+      <div id="top-bar-gold-about-y" className=""></div>
+      <div id="Yahweh_Ben_Yahweh-about-y" className="pg-btm-pd-mod">
+        <img
+          src="/src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/image/Yahweh_Ben_Yahweh.png"
+          alt="Yahweh Ben Yahweh"
+          className="Yahweh_Width"
+        />
+      </div>
+      <div className="sec-bar-about"></div>
+      <div className="pg-link-text">
+        <p className="pg-top-mrgn-mod">YAHWEH BEN YAHWEH</p>
+      </div>
+      <div className="sec-text pg-text-fmt">
+        <p className="">
+          <span className="David">יהוה</span> is God of gods.{" "}
+          <span className="David">יהוה</span> is the covenant God of Israel. He
+          is the God of Abraham, Isaac, and Jacob.{" "}
+          <span className="David">יהוה בן יהוה</span> is the Son of{" "}
+          <span className="David">יהוה</span>.
+        </p>
+        <p className="">
+          <span className="David">יהוה בן יהוה</span> is the Grand Master of the
+          Celestial Lodge, Architect of the Universe, and the Blessed and only
+          Potentate. He is here to set the captives free and to cause them that
+          are bound to stand perpendicular on the square of righteousness. For
+          behold, One greater than Solomon is here!
+        </p>
+      </div>
+      <div className="pg-title-break">
+        <img
+          src="/src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/image/His_Mission.png"
+          alt="His Mission"
+          className="Yahweh_Width_mission"
+        />
+      </div>
+      <div className="sec-text pg-text-fmt">
+        <p className="">
+          In 1979, <span className="David">יהוה בן יהוה</span> came to Miami and
+          became the Spiritual Leader and Founder of The Nation of{" "}
+          <span className="David">יהוה</span>. Although He took a vow of
+          poverty, in seven years He guided The Nation to amass a $250,000,000
+          empire. Under His direction, The Nation of{" "}
+          <span className="David">יהוה</span> has grown to encompass disciples,
+          followers, and supporters in over 1,300 cities within the U.S. and 16
+          foreign countries.
+        </p>
+        <p>
+          His mission is to gather first the lost sheep of the house of Israel,
+          and then the moral people of Earth.{" "}
+          <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span> is here to
+          establish peace and good will toward all God-loving men. He is here to
+          establish the theocratic government of{" "}
+          <span className="David">יהוה</span> by executing the laws, statutes,
+          judgments, and commandments of <span className="David">יהוה</span>.
+        </p>
+        <p>
+          <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span> is bringing
+          about changes in the lives of individuals, and is giving the world the
+          keys to success in life Politically - Economically - Educationally -
+          Socially - and Spiritually.
+        </p>
+      </div>
+      <div className="pg-title-break">
+        <img
+          src="/src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/image/His_Mission.png"
+          alt="His Mission"
+          className="Yahweh_Width_mission"
+        />
+      </div>
+      <div className="sec-text pg-text-fmt">
+        <p>
+          <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span> teaches His
+          disciples and followers to practice charity and benevolence, to
+          protect chastity, to respect the ties of blood and friendship, to
+          adopt the principles and revere the laws of{" "}
+          <span className="David">יהוה</span>, to assist the feeble, guide and
+          open the eyes of the blind, heal the ears of the deaf, raise up the
+          oppressed and downtrodden, shelter the widow and the orphan, guard the
+          altar of <span className="David">יהוה</span>, support the government
+          of <span className="David">יהוה</span>, inculcate morality, promote
+          learning, love moral men, fear <span className="David">יהוה</span>,
+          implore His mercy, work for happiness, and be industrious.
+        </p>
+      </div>
+      <div className="pg-title-break">
+        <img
+          src="/src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/image/His_Mission.png"
+          alt="His Mission"
+          className="Yahweh_Width_mission"
+        />
+      </div>
+      <div className="sec-text pg-text-fmt">
+        <p>
+          <i>
+            <strong>
+              On Monday, May 7, 2007 at 7:55 p.m., our Founder and Savior,{" "}
+              <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span>, completed His
+              first journey on Earth and ascended to stand with His Father,{" "}
+              <span className="David">יהוה</span>, in the Heavens.
+            </strong>
+          </i>
+        </p>
+        <p className="pg-text-ltr-sp-8">
+          Therefore doth My Father, <span className="David">יהוה</span>, love Me (
+          <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span>), because I lay down
+          My life, that I might take it again. No man taketh it from Me, but I
+          lay it down of Myself. I have power to lay it down, and I have power
+          to take it again. This commandment have I received of My Father,{" "}
+          <span className="David">יהוה</span> (John 10:17-18). Greater love hath no
+          man than this, that a man lay down his life for his friends (John
+          15:13). The works that I do in My Father's name, they bear witness of
+          Me (John 10:25).
+        </p>
+        <p className="pg-text-ltr-sp-9">
+          And there was given Me dominion, and glory, and a Kingdom, that all
+          people, nations, and languages, should serve Me: My dominion is an
+          everlasting dominion, which shall not pass away, and My Kingdom that
+          which shall not be destroyed (Daniel 7:14). And My Kingdom and
+          dominion, and the greatness of My Kingdom under the whole Heaven,
+          shall be given to the people of the saints of the Most High, whose
+          Kingdom is an everlasting Kingdom, and all dominions shall serve and
+          obey Me (Daniel 7:27). To{" "}
+          <span className="David">יהוה&nbsp;בּן&nbsp;יהוה</span>&nbsp;be glory and
+          dominion forever and ever. Amen (1 Peter 5:11).
+        </p>
+      </div>
+            <div id="pg-btm-medias" className="py-8">
+        <img className="bottom-media-img mx-auto" src="/src/pages/(sous-page)/About_Yahweh_Ben_Yahweh/image/bottom-media.png" alt="Bottom Media" />
+      </div>
+      Coucou 5
+    </div>
+  );
+}
+export default AboutYahwehBenYahweh;
