8bafd0a2ed74e13804b69b74683a9f5d6e37ce83
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-06-17 04:18:18 -0700

    The_Good_News_of_Yahweh was not finished

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Contact_Us from "./pages/(sous-page)/Contact_Us/Contact_Us";
 import Tetragrammaton from "./pages/(sous-page)/Tetragrammaton/Tetragrammaton";
 import The_Nation_of_Yahweh from "./pages/(sous-page)/The_Nation_of_Yahweh/The_Nation_of_Yahweh";
 import The_Feasts_of_Yahweh from "./pages/(sous-page)/The_Feasts_of_Yahweh/The_Feasts_of_Yahweh";
+import The_Good_News_of_Yahweh from "./pages/(sous-page)/The_Good_News_of_Yahweh/The_Good_News_of_Yahweh";
 
 function App() {
   return (
@@ -32,6 +33,7 @@ function App() {
             <Route path="/Tetragrammaton" element={<Tetragrammaton />} />
             <Route path="/The_Nation_of_Yahweh" element={<The_Nation_of_Yahweh />} />
             <Route path="/The_Feasts_of_Yahweh" element={<The_Feasts_of_Yahweh />} />
+            <Route path="/The_Good_News_of_Yahweh" element={<The_Good_News_of_Yahweh />} />
           </Routes>
         </Layout>
       </Router>
---
+++ src/pages/(sous-page)/The_Good_News_of_Yahweh/The_Good_News_of_Yahweh.tsx
@@ -0,0 +1,16 @@
+import "./style/the_Good_News_of_Yahweh.css";
+import "./style/reponsive.css";
+
+export function The_Good_News_of_Yahweh() {
+  return (
+    <div id="backdrop-news" className="good-news-page">
+      <div id="pg-banner-news" className=""></div>
+      <div id="top-bar-gold-news"></div>
+      <div className="pg-title-text-news">
+        <p className="p-title-text-news">MAGAZINES</p>
+      </div>
+      Coucou 11{" "}
+    </div>
+  );
+}
+export default The_Good_News_of_Yahweh;
