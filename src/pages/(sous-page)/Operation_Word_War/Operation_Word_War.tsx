6cc53d5efc8751cfd236b9303800f24e1c47cd56
LionotKennedy <razafimandimbylionotkennedy2gmail.com>
2025-06-21 23:38:14 -0700

    Operation_Word_War was finished

---
+++ src/pages/(sous-page)/Operation_Word_War/Operation_Word_War.tsx
@@ -0,0 +1,94 @@
+import "./style/operation_Word_War.css";
+import "./style/responsive.css";
+
+export function Operation_Word_War() {
+  return (
+    <>
+      <div id="pg-banner-ope"></div>
+      <div id="top-bar-gold-ope"></div>
+      <div className="sec-text-ope pg-text-ttl-fmt-ope">
+        <p>
+          Fake news was not invented this year!
+          <br />
+          Fake news was not invented this decade!
+          <br />
+          Fake news did not even start in this century!
+        </p>
+      </div>
+      <div className="pg-title-break-ope">
+        <img
+          className="bottom-media-img-ope"
+          src="/src/pages/(sous-page)/Operation_Word_War/image/div-bar.png"
+          alt="divider bar"
+        />
+      </div>
+      <div className="sec-text-ope pg-text-fmt-ope">
+        <p className="p-mrg-fmt-ope">
+          The Meggido Report is the fake news of the 80’s and 90’s, which makes
+          Operation Word War - like all great chronicles - TRUE!
+        </p>
+      </div>
+      <div className="pg-title-break-ope">
+        <img
+          className="bottom-media-img-ope"
+          src="/src/pages/(sous-page)/Operation_Word_War/image/div-bar.png"
+          alt="divider bar"
+        />
+      </div>
+      <div className="sec-text-ope pg-text-fmt-ope">
+        <p className="p-mrg-fmt-ope">
+          This fully documented and annotated story is about a ruthless,
+          vicious, and jealous federal government, and how that government would
+          not allow a black Man to raise up His fellow black Americans from
+          poverty to riches.
+        </p>
+      </div>
+      <div className="pg-title-break-ope">
+        <img
+          className="bottom-media-img-ope"
+          src="/src/pages/(sous-page)/Operation_Word_War/image/div-bar.png"
+          alt="divider bar"
+        />
+      </div>
+      <div className="sec-text-ope pg-text-fmt-ope">
+        <p className="p-mrg-fmt-ope">
+          This Man, Yahweh Ben Yahweh, could not be allowed to succeed, for
+          America is a sacrosanct government, and for some reason only it can
+          help you. Therefore, for showing people that they did not have to be
+          slaves to the system, Yahweh Ben Yahweh was crucified and killed.
+        </p>
+      </div>
+      <div className="pg-title-break-ope">
+        <img
+          className="bottom-media-img-ope"
+          src="/src/pages/(sous-page)/Operation_Word_War/image/div-bar.png"
+          alt="divider bar"
+        />
+      </div>
+      <div className="sec-text-ope pg-text-fmt-ope">
+        <p className="p-mrg-fmt-ope">
+          The documents linked on this page should be read in a quiet,
+          comfortable environment to fully appreciate the terror of this
+          government and how it can and will affect you ...
+        </p>
+      </div>
+      <div id="Authentic">
+        <a href="pdf/Operation_Word_War_Web.pdf" target="_blank">
+          <img
+            className="img-size-ope"
+            src="/src/pages//(sous-page)/Operation_Word_War/image/Authentic.jpg"
+            alt="Authentic Operation Word War Web pdf document"
+          />
+        </a>
+      </div>
+      <div>
+        <img
+          className="bottom-media-img-ope"
+          src="/src/pages/(sous-page)/Operation_Word_War/image/bottom-media.jpg"
+        />
+      </div>
+      {/* Coucou 14 */}
+    </>
+  );
+}
+export default Operation_Word_War;
